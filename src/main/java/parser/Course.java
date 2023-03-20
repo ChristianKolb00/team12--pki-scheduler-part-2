12fd9c46f0218dad5a53c5989a1cb5d2b88fe009
Jcwillson <jwillson@unomaha.edu>
2023-03-20 11:46:53 -0500

    Edited web calls

--- src/main/java/parser/Course.java
+++ src/main/java/parser/Course.java
@@ -67,11 +67,11 @@ public class Course extends Line{
 	protected void processWebOriginal()
 	{
 		//Decide what data to put into webOriginal
-		String[] webOriginal = {line[1], line[8] + "-" + line[9], line[11], line[13], line[14], line[15], line[19], line[28],line[29], String.valueOf(aggEnrollOriginal)};
+		String[] webOriginal = {line[8] + "-" + line[9], line[11], line[13], line[14], line[15], line[28],line[29], String.valueOf(aggEnrollOriginal)};
 	}
 	private void processWeb() {
 		//Decide what to put in display
-		String[] web = {diff[1], diff[8] + "-" + diff[9], diff[11], diff[13], diff[14], diff[15], diff[19], diff[28], diff[29], String.valueOf(aggEnroll)};
+		String[] web = {diff[8] + "-" + diff[9], diff[11], diff[13], diff[14], diff[15], diff[28], diff[29], String.valueOf(aggEnroll)};
 		changed = false;
 	}
 	
