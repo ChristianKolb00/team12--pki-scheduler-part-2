2f5c1a796888401f30c1ba9732b5694bc26ef4d7
Jcwillson <jwillson@unomaha.edu>
2023-03-22 15:55:09 -0500

    Added comments to TimeTable and restoration methods

--- src/main/java/parser/TimeTable.java
+++ src/main/java/parser/TimeTable.java
@@ -1,14 +1,14 @@
 package parser;
 
 public class TimeTable {
+	//2d array representing [day][time in 15s]
 	private Course[][] table;
 	private Course blocker;
 	public TimeTable()
 	{
 		table = new Course[Constants.NUM_DAYS][Constants.NUM_FIFTEENS];
-		//Preset table loops
 		blocker = new Course(null);//Might need special handling to create a good readable blocker?
-		//Use a blocker course to blackout times
+		//Use a blocker course to blackout invalid times
 		for(int h=0; h <= table.length; h++)
 		{
 			for(int i = 0; i < 33; i++)
@@ -17,7 +17,8 @@ public class TimeTable {
 				table[h][j]=blocker;
 		}
 	}
-	//Returns true if all slots for course and slot before and after are available
+	
+	//Returns true if all slots for course and slot before and after are available on specified day
 	public boolean checkAvailable(int d, int t, int du)
 	{
 		for (int k = t-1; k < t + du + 1; k++)
@@ -29,7 +30,6 @@ public class TimeTable {
 	
 	//Sets a Course into the specified time slots
 	//Double checks against checkAvailable, throwing ScheduleException if issue
-	//Additionally release prior held time slots if applicable
 	public void set(Course c) throws ScheduleException
 	{
 		int[] td = c.parseTime();
@@ -43,27 +43,50 @@ public class TimeTable {
 		//Add new time
 		for(int j = 0; j < days.length; j++)
 			for(int k = td[0]; k < td[0] + td[1]; k++)
-				table[j][k]=c;
+				table[days[j]][k]=c;
 	}
 	
-	public void release(Course c)
+	//Unsets a courses original time
+	public void release(Course c) throws ScheduleException
 	{
 		int[] td = c.parseOriginalTime();
 		int[] days = c.parseOriginalDay();
 		for(int j = 0; j < days.length; j++)
 			for(int k = td[0]; k < td[0] + td[1]; k++)
 				//Check to make sure its not already descheduled
-				if(table[j][k] == c)
-					table[j][k]=null;
+				if(table[days[j]][k] == c)
+					table[days[j]][k]=null;
+				else
+					throw new ScheduleException("Course already descheduled");
 	}
 	
-	public void restore(Course c)
+	
+	//After analysis is completed, this will release the slots the Course was changed too
+	public void restoreRelease(Course c) throws ScheduleException
+	{
+		int[] td = c.parseTime();
+		int[] days = c.parseDay();
+		for(int j = 0; j < days.length; j++)
+			for(int k = td[0]; k < td[0] + td[1]; k++)
+				//Check to make sure its not already descheduled
+				if(table[days[j]][k] == c)
+					table[days[j]][k]=null;
+				else
+					throw new ScheduleException("Course already descheduled");
+	}
+	
+	//After analysis is completed, AND restoreRelease, this will set the Course's original time slots
+	public void restore(Course c) throws ScheduleException
 	{
 		int[] td = c.parseOriginalTime();
 		int[] days = c.parseOriginalDay();
 		//No extra check needed because this is only ever called after releaseTime(c)
 		for(int j = 0; j < days.length; j++)
 			for(int k = td[0]; k < td[0] + td[1]; k++)
-				table[j][k]=c;
+			{
+				if(table[days[j]][k] != null)
+					throw new ScheduleException("Improper restore order");
+				table[days[j]][k]=c;
+			}
 	}
 }
