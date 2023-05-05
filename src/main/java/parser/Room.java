6d67a03d6e30fcee92f11919c068a8032b0e4e49
Jcwillson <jwillson@unomaha.edu>
2023-05-05 12:01:57 -0500

    Room comments

--- src/main/java/parser/Room.java
+++ src/main/java/parser/Room.java
@@ -1,11 +1,32 @@
 package parser;
 
+/**
+ * Room specific version of time table, with needed attribute storage
+ * @author jcwga
+ *
+ */
+
 public class Room extends TimeTable{
+	/*
+	 * The name of the building the Room is in
+	 */
 	private String building;
+	/*
+	 * The alphanumeric designator of this Room
+	 */
 	private String roomNum;
-	private int cap, comp;
+	/*
+	 * The person capacity of this Room
+	 */
+	private int cap;
+	/*
+	 * The computer capacity of this Room
+	 */
+	private int comp;
 	
-	//Generalized explicit constructor
+	/*
+	 * General constructor to create the TimeTable and fill attributes
+	 */
 	public Room(String b, String n, int ca, int co)
 	{
 		super();
@@ -15,7 +36,9 @@ public class Room extends TimeTable{
 		comp = co;
 	}
 	
-	//Special case constructor from constants
+	/*
+	 * Specialized constructor that uses the Constant array structure to create a Room
+	 */
 	public Room(Object[] a)
 	{
 		super();
@@ -25,21 +48,38 @@ public class Room extends TimeTable{
 		comp = (int) a[2];
 	}
 	
+	//GETTERS
+	/**
+	 * Accessor for the building name of this Room
+	 * @return building name of this Room
+	 */
 	public String getBuilding()
 	{
 		return building;
 	}
 	
+	/**
+	 * Accessor for the alphanumeric designator of this Room
+	 * @return designator of this Room
+	 */
 	public String getRoomNumber()
 	{
 		return roomNum;
 	}
 	
+	/**
+	 * Accessor for the person capacity of this Room
+	 * @return the number of people this Room can hold
+	 */
 	public int getCapacity()
 	{
 		return cap;
 	}
 	
+	/**
+	 * Accessor for the computer capacity of this Room
+	 * @return the number of computers in this Room
+	 */
 	public int getComputerCapacity()
 	{
 		return comp - 1;
