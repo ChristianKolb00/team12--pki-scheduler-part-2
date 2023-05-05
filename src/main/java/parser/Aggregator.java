45c4ffa9c8d08e2126372bb96fd2695201b9082b
Jcwillson <jwillson@unomaha.edu>
2023-05-05 11:32:25 -0500

    Aggregator comments

--- src/main/java/parser/Aggregator.java
+++ src/main/java/parser/Aggregator.java
@@ -2,12 +2,42 @@ package parser;
 
 import java.util.ArrayList;
 
+/**
+ * The master controller class, that handles taking in multiple CSVs, turning each into its own Document. Creates a master list of Rooms, finds all Courses, filters Courses
+ * to relevant ones, schedules those Courses, and creates an easy access list. Also contains lookup methods for Courses and Rooms.
+ * @author jwillson
+ *
+ */
 public class Aggregator {
+	/**
+	 * An array of all documents parsed
+	 */
 	private Document[] documents;
-	private Course[] allCourses, courses;
+	/**
+	 * An array of all Courses parsed
+	 */
+	private Course[] allCourses;
+	/**
+	 * An array of all Courses that are relevant to the application
+	 */
+	private Course[] courses;
+	/**
+	 * An array of all valid Rooms
+	 */
 	private Room[] rooms;
+	/**
+	 * An array of all valid Professors *UNIMPLEMENTED*
+	 */
 	private Professor[] professors;
+	/**
+	 * An array of all relevant Course names with a 1:1 mapping for ease of lookup and display
+	 */
 	private String[] coursesNames;
+	
+	/**
+	 * Constructor of aggregator, uses an array of file paths as Strings
+	 * @param paths - an array of file paths
+	 */
 	public Aggregator(String[] paths)
 	{
 		rooms = createRooms();
@@ -22,15 +52,41 @@ public class Aggregator {
 		processCourseNames();
 	}
 	
+	
+	//GETTERS
+	/**
+	 * Accessor for the array of relevant Courses
+	 * @return all relevant Courses
+	 */
 	public Course[] getCourses()
 	{
 		return courses;
 	}
+	
+	/**
+	 * Accessor for the array of all Course names
+	 * @return all relevant Course names
+	 */
 	public String[] getCourseNames()
 	{
 		return coursesNames;
 	}
 	
+	/**
+	 * Accessor for the array of all valid Rooms
+	 * @return all valid Rooms
+	 */
+	public Room[] getRooms()
+	{
+		return rooms;
+	}
+	
+	//Finders
+	/**
+	 * Lookup by String to find the associated relevant Course
+	 * @param name - the name of the Course being looked for in XXXX YYYY-ZZZ format
+	 * @return the associated relevant Course object
+	 */
 	public Course findCourse(String name)
 	{
 		for(int i = 0; i < coursesNames.length; i++)
@@ -40,7 +96,11 @@ public class Aggregator {
 		}
 		return null;
 	}
-	
+	/**
+	 * Lookup by String to find the associated valid Room
+	 * @param name - the number of the room
+	 * @return the associated Room object
+	 */
 	public Room findRoomByNum(String name)
 	{
 		for(int i = 0; i < rooms.length; i++)
@@ -51,23 +111,27 @@ public class Aggregator {
 		return null;
 	}
 	
-	private void processCourseNames()
+	//Helpers
+	/**
+	 * Creates an array of Rooms based off the defined parameters in Constants
+	 * @return all valid Rooms
+	 */
+	private Room[] createRooms()
 	{
-		ArrayList<String> names = new ArrayList<String>();
-		for (int i = 0; i < courses.length; i++) {
-			names.add(( courses[i]).getCourseSection());
+		ArrayList<Room> room = new ArrayList<Room>();
+		for(int i = 0; i < Constants.pki.length; i++)
+		{
+			room.add(new Room(Constants.pki[i]));
 		}
-		String[] ret = new String[names.size()];
-		names.toArray(ret);
-		coursesNames = ret;
+		Room[] ret = new Room[room.size()];
+		room.toArray(ret);
+		return ret;
 	}
 	
-	public Room[] getRooms()
-	{
-		return rooms;
-	}
-	
-	//Iterate through all documents finding the Lines that are course flagged
+	/**
+	 * Iterates through all Lines in all Documents, finding ones that are Courses and making an array of those
+	 * @return all Courses
+	 */
 	private Course[] findCourses() {
 		ArrayList<Course> course = new ArrayList<Course>();
 		//Iterate through entire set of all lines
@@ -87,6 +151,9 @@ public class Aggregator {
 		return ret;
 	}
 	
+	/**
+	 * Iterates through all Courses, finding those Courses that either have a parent or child and linking them together
+	 */
 	private void parentChildLinkage()
 	{
 		for(int i = 0; i < allCourses.length; i++)
@@ -110,6 +177,10 @@ public class Aggregator {
 		}
 	}
 	
+	/**
+	 * Iterates through all Courses, filtering to those relevant to the application
+	 * @return all Courses that are scheduled in PKI
+	 */
 	private Course[] refineCourses()
 	{
 		ArrayList<Course> course = new ArrayList<Course>();
@@ -124,18 +195,9 @@ public class Aggregator {
 		return ret;
 	}
 	
-	private Room[] createRooms()
-	{
-		ArrayList<Room> room = new ArrayList<Room>();
-		for(int i = 0; i < Constants.pki.length; i++)
-		{
-			room.add(new Room(Constants.pki[i]));
-		}
-		Room[] ret = new Room[room.size()];
-		room.toArray(ret);
-		return ret;
-	}
-	
+	/**
+	 * Iterates through all Courses at PKI, scheduling them into the TimeTables of their Rooms
+	 */
 	private void scheduleCourses()
 	{
 		for(int i = 0; i < courses.length; i++)
@@ -151,6 +213,23 @@ public class Aggregator {
 		}
 	}
 	
+	/**
+	 * Iterates through all Courses at PKI, making a list of their Course+Section name that has a 1:1 correspondence by index to the Course list
+	 */
+	private void processCourseNames()
+	{
+		ArrayList<String> names = new ArrayList<String>();
+		for (int i = 0; i < courses.length; i++) {
+			names.add(( courses[i]).getCourseSection());
+		}
+		String[] ret = new String[names.size()];
+		names.toArray(ret);
+		coursesNames = ret;
+	}
+	
+	/**
+	 * Standard output format for debugging
+	 */
 	@Override
 	public String toString()
 	{
